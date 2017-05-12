0343c35ff657d00c736367f8bf2c90ee42e8834a
wongsw <wong.songwei@gmail.com>
2017-05-12 14:51:06 -0500

    Added Vive code

--- CodeV2Vive/CodeV2Vive.ino
+++ CodeV2Vive/CodeV2Vive.ino
@@ -1,7 +1,36 @@
+#include <avr/interrupt.h>
+#include <math.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 #define LEDPin 13
+#define V1PIN 9 // the signal from the front sensor
+#define V2PIN 8 // the signat from the back sensor
+#define DEG_PER_US 0.0216 // equal to (180 deg) / (8333 us)
+#define LIGHTHOUSEHEIGHT 6.0 // in feet
+
+// structure to store the sensor data
+typedef struct {
+  unsigned long changeTime[11];
+  double horzAng;
+  double vertAng;
+  int useMe;
+  int collected;
+} viveSensor;
+
+// variables for the sensor data and filter
+volatile viveSensor V1;
+volatile viveSensor V2;
+int state = 0;
+double xPos1, yPos1;
+double xPos2, yPos2;
+double xOld1 = 0, yOld1 = 0, xFilt1 = 0, yFilt1 = 0;
+double xOld2 = 0, yOld2 = 0, xFilt2 = 0, yFilt2 = 0;
+
+// variables for the xbee data
+char msg[100];
+int msg_index = 0;
+float xOpponent, yOpponent;
 
 int photoTran = 35; //A16
 int reading = 0;
@@ -89,6 +118,25 @@ void setup(){
   attachInterrupt(buttonL, buttonLCallback, RISING);
   randomSeed(analogRead(0));
 
+  Serial4.begin(9600); // to listen to the xbee
+  pinMode(13, OUTPUT); // to blink the led on pin 13
+  pinMode(V1PIN, INPUT); // to read the front sensor
+  pinMode(V2PIN, INPUT); // to read the back sensor
+
+  // initialize the sensor variables
+  V1.horzAng = 0;
+  V1.vertAng = 0;
+  V1.useMe = 0;
+  V1.collected = 0;
+
+  V2.horzAng = 0;
+  V2.vertAng = 0;
+  V2.useMe = 0;
+  V2.collected = 0;
+  // interrupt on any sensor change
+  attachInterrupt(digitalPinToInterrupt(V1PIN), ISRV1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(V2PIN), ISRV2, CHANGE);
+  
   Serial.begin(9600);
 }
 
@@ -109,8 +157,8 @@ void loop(){
   //normal operation
   newDistF = ultrasound(echoUSF,trigUSF);
   newDistB = ultrasound(echoUSB, trigUSB);
-//  newDistR = ultrasound(echoUSR, trigUSR);
-//  newDistL = ultrasound(echoUSL, trigUSL);
+  newDistR = ultrasound(echoUSR, trigUSR);
+  newDistL = ultrasound(echoUSL, trigUSL);
   if (newDistF != 0 && newDistF < 1000){
     currDistF = newDistF;
   }
@@ -123,6 +171,7 @@ void loop(){
     if (newDistL != 0 && newDistL < 1000){
     currDistL = newDistL;
   }
+
 
 //random movement
 //changes direction after every random motion, including when the bot hits a wall with both sides clear
@@ -164,6 +213,77 @@ void loop(){
     accelerate(dirForward);
   }
   delay(100);
+
+  /// Vive and xbee code
+// see if the xbee has sent any data
+  /*if (Serial4.available() > 0) {
+    msg[msg_index] = Serial4.read();
+    // if you get a newline, there is data to read
+    if (msg[msg_index] == '\n') {
+      msg_index = 0;
+      // data is in the format of two floats seperated by spaces
+      sscanf(msg, "%f %f", &xOpponent, &yOpponent);
+      Serial.print("op: ");
+      Serial.print(xOpponent);
+      Serial.print(" ");
+      Serial.println(yOpponent);
+    }
+    else {
+      // did not get a newline yet, just store for later
+      msg_index++;
+      if (msg_index == 100) {
+        msg_index = 0;
+      }
+    }
+  }*/
+
+  // if the sensor data is new
+  if (V1.useMe == 1) {
+    V1.useMe = 0;
+
+    // calculate the position and filter it
+    xPos1 = tan((V1.vertAng - 90.0) * DEG_TO_RAD) * LIGHTHOUSEHEIGHT;
+    yPos1 = tan((V1.horzAng - 90.0) * DEG_TO_RAD) * LIGHTHOUSEHEIGHT;
+    xFilt1 = xOld1 * 0.5 + xPos1 * 0.5;
+    yFilt1 = yOld1 * 0.5 + yPos1 * 0.5;
+    xOld1 = xFilt1;
+    yOld1 = yFilt1;
+    Serial.print("Front: ");
+    Serial.print(xFilt1);
+    Serial.print(" ");
+    Serial.println(yFilt1);
+
+    // calculate the position and filter it
+    xPos2 = tan((V2.vertAng - 90.0) * DEG_TO_RAD) * LIGHTHOUSEHEIGHT;
+    yPos2 = tan((V2.horzAng - 90.0) * DEG_TO_RAD) * LIGHTHOUSEHEIGHT;
+    xFilt2 = xOld2 * 0.5 + xPos2 * 0.5;
+    yFilt2 = yOld2 * 0.5 + yPos2 * 0.5;
+    xOld2 = xFilt2;
+    yOld2 = yFilt2;
+    Serial.print("Back: ");
+    Serial.print(xFilt2);
+    Serial.print(" ");
+    Serial.println(yFilt2);
+
+    // blink the led so you can tell if you are getting sensor data
+    digitalWrite(13, state);
+    if (state == 1) {
+      state = 0;
+    }
+    else {
+      state = 1;
+    }
+  }
+  /*if (xFilt1-xFilt2>0 && yFilt1-yFilt2>0){
+    Serial.println("Front Right");}
+  else if (xFilt1-xFilt2>0 && yFilt1-yFilt2<0){
+    Serial.println("Back Right");}
+  else if (xFilt1-xFilt2<0 && yFilt1-yFilt2>0){
+    Serial.println("Front Left");}
+  else if (xFilt1-xFilt2<0 && yFilt1-yFilt2<0){
+    Serial.println("Back Left");}
+    */
+///end of Vive and xbee code
 }
 
 int slowDown(bool dirForward) {
@@ -285,3 +405,55 @@ void buttonLCallback () {
   }
 }
 
+
+
+// the sensor interrupt
+void ISRV1() {
+  // get the time the interrupt occured
+  unsigned long mic = micros();
+  int i;
+
+  // shift the time into the buffer
+  for (i = 0; i < 10; i++) {
+    V1.changeTime[i] = V1.changeTime[i + 1];
+  }
+  V1.changeTime[10] = mic;
+
+  // if the buffer is full
+  if (V1.collected < 11) {
+    V1.collected++;
+  }
+  else {
+    // if the times match the waveform pattern
+    if ((V1.changeTime[1] - V1.changeTime[0] > 7000) && (V1.changeTime[3] - V1.changeTime[2] > 7000) && (V1.changeTime[6] - V1.changeTime[5] < 50) && (V1.changeTime[10] - V1.changeTime[9] < 50)) {
+      V1.horzAng = (V1.changeTime[5] - V1.changeTime[4]) * DEG_PER_US;
+      V1.vertAng = (V1.changeTime[9] - V1.changeTime[8]) * DEG_PER_US;
+      V1.useMe = 1;
+    }
+  }
+}
+
+void ISRV2() {
+  // get the time the interrupt occured
+  unsigned long mic = micros();
+  int i;
+
+  // shift the time into the buffer
+  for (i = 0; i < 10; i++) {
+    V2.changeTime[i] = V2.changeTime[i + 1];
+  }
+  V2.changeTime[10] = mic;
+
+  // if the buffer is full
+  if (V2.collected < 11) {
+    V2.collected++;
+  }
+  else {
+    // if the times match the waveform pattern
+    if ((V2.changeTime[1] - V2.changeTime[0] > 7000) && (V2.changeTime[3] - V2.changeTime[2] > 7000) && (V2.changeTime[6] - V2.changeTime[5] < 50) && (V2.changeTime[10] - V2.changeTime[9] < 50)) {
+      V2.horzAng = (V2.changeTime[5] - V2.changeTime[4]) * DEG_PER_US;
+      V2.vertAng = (V2.changeTime[9] - V2.changeTime[8]) * DEG_PER_US;
+      V2.useMe = 1;
+    }
+  }
+}
