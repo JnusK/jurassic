7b98be9c6b04335a5be2e707cc0d3f453b6a647d
Janus <jnus@live.com.sg>
2017-05-12 00:39:33 -0500

    Add files via upload

--- interrupt.ino
+++ interrupt.ino
@@ -1,12 +1,14 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define LEDPin 13
-const int intF = 36;
+const int intF = 39;
 const int intB = 37;
 const int intL = 38;
-const int intR = 39;
+const int intR = 36;
 const int buttonR = 5;
 const int buttonL = 4;
+
+const int pin = 33;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,12 +18,14 @@ void setup() {
   pinMode(intR, INPUT);
   pinMode(buttonR, INPUT);
   pinMode(buttonL, INPUT);
-  pinMode (LEDPin,OUTPUT);
+  pinMode(LEDPin, OUTPUT);
+  pinMode(pin, OUTPUT);
   
-  attachInterrupt(intF, ISRF, RISING);
-  attachInterrupt(intB, ISRB, RISING);
-  attachInterrupt(intL, ISRL, RISING);
-  attachInterrupt(intR, ISRR, RISING);
+  
+  attachInterrupt(intF, ISR, RISING);
+  attachInterrupt(intB, ISR, RISING);
+  attachInterrupt(intL, ISR, RISING);
+  attachInterrupt(intR, ISR, RISING);
   attachInterrupt(buttonR, ISRbuttonR, RISING);
   attachInterrupt(buttonL, ISRbuttonL, RISING);
 }
@@ -29,24 +33,38 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
+  //digitalWrite(LEDPin, HIGH);
+  digitalWrite(pin, HIGH);
+  delay(5000);
+  digitalWrite(pin, LOW);
   digitalWrite(LEDPin, HIGH);
-  delay(100);
+  delay(5000);
+}
+
+void ISR(){
+  digitalWrite(LEDPin, LOW);
+  Serial.println("Triggered");
+  //digitalWrite(pin, LOW);
 }
 
 void ISRF(){
   digitalWrite(LEDPin, LOW);
+  digitalWrite(pin, LOW);
 }
 
 void ISRB(){
   digitalWrite(LEDPin, LOW);
+  digitalWrite(pin, LOW);
 }
 
 void ISRL(){
   digitalWrite(LEDPin, LOW);
+  digitalWrite(pin, LOW);
 }
 
 void ISRR(){
   digitalWrite(LEDPin, LOW);
+  digitalWrite(pin, LOW);
 }
 
 void ISRbuttonR(){
